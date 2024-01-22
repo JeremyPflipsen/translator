72aa12010f17a9f14ec422ffa922817ea634d17c
JeremyPflipsen <jeremypflipsen@gmail.com>
2024-01-21 22:38:37 -0600

    Setup tailwind and layout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,18 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ToolbarComponent } from "./toolbar/toolbar.component";
+import { ToolbarComponent } from './toolbar/toolbar.component';
 import { MaterialModule } from './material/material.module';
+import { FormControl } from '@angular/forms';
+
+interface Language {
+  value: string;
+}
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [RouterOutlet, ToolbarComponent, MaterialModule]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, ToolbarComponent, MaterialModule],
 })
 export class AppComponent {
   title = 'frontend';
-  firstLanguageText:string = 'To dream the impossible dream, that is my quest.'
-  secondLanguageText:string = 'El soñar... el sueño imposible'
-  languages: string[] = ['English', 'Spanish', 'French', 'Japanese']
+  firstLanguageText: string =
+    'To dream the impossible dream, that is my quest.';
+  secondLanguageText: string =
+    'El soñar... el sueño imposible, Ésta es mi búsqueda';
+
+  selectedFirstLanguage: string = 'English';
+  selectedSecondLanguage: string = 'Spanish';
+
+  languages: Language[] = [
+    { value: 'English' },
+    { value: 'Spanish' },
+    { value: 'French' },
+    { value: 'Japanese' },
+  ];
 }
--- frontend/src/app/material/material.module.ts
+++ frontend/src/app/material/material.module.ts
@@ -2,8 +2,15 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { TextFieldModule } from '@angular/cdk/text-field';
 import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
-const MaterialComponents = [MatToolbarModule, TextFieldModule, MatSelectModule];
+const MaterialComponents = [
+  MatToolbarModule,
+  TextFieldModule,
+  MatSelectModule,
+  ReactiveFormsModule,
+  FormsModule,
+];
 
 @NgModule({
   imports: [MaterialComponents],
