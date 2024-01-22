edf581358ed554787ba509a748b7ca905f6973f3
JeremyPflipsen <jeremypflipsen@gmail.com>
2024-01-22 16:54:00 -0600

    Add timeout for translate call

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -9,18 +9,18 @@ interface Language {
 }
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [RouterOutlet, ToolbarComponent, MaterialModule]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, ToolbarComponent, MaterialModule],
 })
 export class AppComponent {
   title = 'frontend';
   firstLanguageText: string =
     'To dream the impossible dream, that is my quest.';
   secondLanguageText: string =
-    'El soñar... el sueño imposible, Ésta es mi búsqueda';
+    'El soñar... el sueño imposible, Ésta es mi búsqueda.';
 
   selectedFirstLanguage: string = 'English';
   selectedSecondLanguage: string = 'Spanish';
@@ -31,4 +31,45 @@ export class AppComponent {
     { value: 'French' },
     { value: 'Japanese' },
   ];
+
+  timeOfLastUserInput: Date = new Date();
+  minTimeBetweenCalls: number = 3000; //in milliseconds
+  timeSinceLastUserInput: number = 0
+  blockToTime: Date = new Date()
+  sufficientTimeHasPassed(): boolean {
+    let now: Date = new Date();
+    this.timeSinceLastUserInput = now.getTime() - this.timeOfLastUserInput.getTime();
+    return this.timeSinceLastUserInput > this.minTimeBetweenCalls
+  }
+
+  onUserInput() {
+    // console.log("USERINPUT. now = ", new Date().getTime())
+    //Wait some time between calls to save processing
+    let timeOfCurrentUserInput: Date = new Date()
+    let timeToWait: number = 0;
+    if (!this.sufficientTimeHasPassed()) {
+      timeToWait = this.minTimeBetweenCalls - this.timeSinceLastUserInput;
+    }
+
+    // console.log("SETTIMEOUT timeToWait: ", timeToWait)
+    this.blockToTime = new Date(timeOfCurrentUserInput.getTime() + timeToWait)
+    this.timeOfLastUserInput = timeOfCurrentUserInput
+    // console.log("blockToTime:", this.blockToTime.getTime())
+
+    setTimeout(() => {
+      this.callTranslateApi()
+    }, timeToWait);
+  }
+
+  callTranslateApi() {
+    if (new Date().getTime() < this.blockToTime.getTime()) {
+      // console.log("NOT ENOUGH TIME PASSED. now = ", new Date().getTime())
+      // console.log("TimeofLastUserInput: ", this.timeOfLastUserInput.getTime())
+      return
+    }
+
+    //do logic here
+    console.log("API CALL. now = ", new Date().getTime())
+  }
+
 }
